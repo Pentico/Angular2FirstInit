a7587605878b41c940a3907dffd019ecdf0f3b51
Pentico <morulaneat@gmail.com>
2017-03-12 13:20:11 +0200

    Component Encapsulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LeftComponent } from './left/left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftComponent
   ],
   imports: [
     BrowserModule,
